17b5d49a00ea22512825739fe31a4fb79516f095
sunakshitejwani <sunakshitejwani@gmail.com>
2021-08-07 22:50:57 +0530

    initial commit of keeper notes app

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>Title of note</h1>
+        <p>Content of note</p>
+    </div>
+}
+
+export default Note;
